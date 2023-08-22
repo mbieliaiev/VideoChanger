a9da7fef2479e57b81b25a2ecfed1001da8eaae6
Mark Bieliaiev <belyaev.mark@gmail.com>
2023-08-22 22:50:08 +0300

    Add video brightness changing

---
+++ VideoChanger/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+
+        // Accessing configuration settings
+        string inputFileAddress = configuration["AppSettings:InputFileAddress"];
+        string outputFileName = configuration["AppSettings:OutputFileName"];
+        string outputDir = configuration["AppSettings:OutputDir"];
+        int numberFilesGenerated = int.Parse(configuration["AppSettings:NumberFilesGenerated"]);
+        int brightnessMinPercent = int.Parse(configuration["AppSettings:BrightnessMinPercent"]);
+        int brightnessMaxPercent = int.Parse(configuration["AppSettings:BrightnessMaxPercent"]);
+
+        double brightnessChangeMin = 1 + brightnessMinPercent / 100.0;
+        double brightnessChangeMax = 1 - brightnessMaxPercent / 100.0;
+
+        var random = new Random();        
+
+        for (int i = 0; i < numberFilesGenerated; i++)
+        {
+            var videoChanger = new VideoChanger.VideoChanger(inputFileAddress);
+            double randomChangeNumber = random.NextDouble() * (brightnessChangeMax - brightnessChangeMin) + brightnessChangeMin;
+            videoChanger.AddBrightness($"{outputDir}\\{outputFileName}-{i + 1}.mp4", randomChangeNumber);
+        }
+    }
+}
---
+++ VideoChanger/VideoChanger.cs
@@ -0,0 +1,65 @@
+﻿using Emgu.CV;
+using System.Drawing;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace VideoChanger
+{
+    public class VideoChanger
+    {
+        private string _videoFilePath;
+        public VideoChanger(string videoFilePath) {
+            _videoFilePath = videoFilePath;
+        }
+
+        public void AddBrightness(string outputFileName, double randomChangeNumber)
+        {
+            using (VideoCapture videoCapture = new VideoCapture(_videoFilePath))
+            {
+                if (!videoCapture.IsOpened)
+                {
+                    Console.WriteLine("Error opening video file.");
+                    return;
+                }
+
+                // Get video properties
+                int frameWidth = (int)videoCapture.Get(CapProp.FrameWidth);
+                int frameHeight = (int)videoCapture.Get(CapProp.FrameHeight);
+                double fps = videoCapture.Get(CapProp.Fps);
+
+                // Create an output window
+                CvInvoke.NamedWindow("Processed Video", WindowFlags.Normal);
+
+                VideoWriter videoWriter = new VideoWriter(outputFileName, VideoWriter.Fourcc('X', '2', '6', '4'), fps, new Size(frameWidth, frameHeight), true);
+
+                while (true)
+                {
+                    Mat frame = new Mat();
+                    if (!videoCapture.Read(frame))
+                        break;                    
+
+                    // Adjust brightness of the frame
+                    frame = randomChangeNumber * frame;
+
+                    // Display the frame
+                    CvInvoke.Imshow("Processed Video", frame);
+
+                    // Write the frame to the output video file
+                    videoWriter.Write(frame);
+
+                    // Exit loop if 'Esc' key is pressed
+                    if (CvInvoke.WaitKey((int)(1000 / fps)) == 27)
+                        break;
+
+                    frame.Dispose();
+                }
+
+                videoWriter.Dispose();
+            }
+
+            CvInvoke.DestroyAllWindows();
+
+        }
+    }
+}
+
